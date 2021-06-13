d5cb774a8b3b641f2a5c824acfe2584eb299a89e
Wayne Jiao <waynejiaozl@gmail.com>
2021-06-12 20:55:24 -0500

    red black tree node change

--- Tree/rbtreenode.h
+++ Tree/rbtreenode.h
@@ -1,20 +1,18 @@
 #ifndef RBTREENODE_H
 #define RBTREENODE_H
 
-#include "string.h"
-
 template<typename Node> class RBTree;
 template<typename Node>
 class RBTreeNode {
 	friend class RBTree<Node>;
 public:
 	RBTreeNode(const Node &d):leftPtr(0), rightPtr(0), data(0), color(null){}
-	Node getData() const{return data;}
+	Node getData() const{return this->data;}
 private:
 	RBTreeNode<Node> *leftPtr;
 	RBTreeNode<Node> *rightPtr;
 	Node data;
-	string color;
+	int color;
 };
 
 #endif
